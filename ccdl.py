d7a38aaa0003d62cc0bfc5e7e61a9bb029433555
Drovosek01 <drovosek01tnt@gmail.com>
2026-03-01 07:03:25 +0300

    Marked current version as v0.3.0

--- ccdl.py
+++ ccdl.py
@@ -30,8 +30,7 @@ except ImportError:
 session = requests.sessions.Session()
 
 VERSION = 4
-VERSION_STR = '0.2.1'
-
+VERSION_STR = '0.3.0'
 
 # path to dir where current file
 script_dir = os.path.dirname(os.path.realpath(__file__))
